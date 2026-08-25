6a423ba4ad4f00fd04e234cedebd709b8e19a3f7
juanfernandez2306 <juanfernandez2306@gmail.com>
2026-08-25 14:29:06 -0400

    agregando clase de respuesta de datos

---
+++ src/Shared/Http/HttpStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Http;
+
+class HttpStatus
+{
+    public const OK = 200;
+    public const CREATED = 201;
+    public const BAD_REQUEST = 400;
+    public const UNAUTHORIZED = 401;
+    public const FORBIDDEN = 403;
+    public const NOT_FOUND = 404;
+    public const METHOD_NOT_ALLOWED = 405;
+    public const CONFLICT = 409;
+    public const UNPROCESSABLE_ENTITY = 422;
+    public const INTERNAL_SERVER_ERROR = 500;
+}
