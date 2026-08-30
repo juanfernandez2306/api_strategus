04ade5bf2bf0a11b5852246b638c36d57313e088
juanfernandez2306 <juanfernandez2306@gmail.com>
2026-08-30 08:32:47 -0400

    ajustando rutas

--- src/Users/Routes.php
+++ src/Users/Routes.php
@@ -1,7 +1,6 @@
 <?php
 
 use Slim\Routing\RouteCollectorProxy;
-use Psr\Container\ContainerInterface;
 use App\Middleware\RateLimitMiddleware;
 use App\Users\Repositories\Auth\RateLimitCacheRepository;
 use App\Users\Actions\Auth\RegisterAction;
@@ -11,19 +10,20 @@ use App\Users\Actions\Auth\ResendVerificationEmailAction;
 use App\Users\Actions\Auth\SendPasswordResetEmailAction;
 use App\Users\Actions\Auth\ResetPasswordAction;
 
-return function (RouteCollectorProxy $group, ContainerInterface $container) {
+return function (RouteCollectorProxy $group) {
 
+    $container = $group->getContainer();
     $cacheRepo = $container->get(RateLimitCacheRepository::class);
 
+
     $authLimiter = new RateLimitMiddleware($cacheRepo, limit: 5, windowSeconds: 60);
+    $mailLimiter = new RateLimitMiddleware($cacheRepo, limit: 3, windowSeconds: 300);
+
 
     $group->post('/register', RegisterAction::class)->add($authLimiter);
     $group->post('/login', LoginAction::class)->add($authLimiter);
 
-    $mailLimiter = new RateLimitMiddleware($cacheRepo, limit: 3, windowSeconds: 300);
-
     $group->group('/email', function (RouteCollectorProxy $emailGroup) use ($cacheRepo) {
-
         $emailGroup->get('/verify', VerifyEmailAction::class)
             ->add(new RateLimitMiddleware($cacheRepo, limit: 10, windowSeconds: 60));
 
