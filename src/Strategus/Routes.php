ba1fc1f3198f5fc71ee92d8637e21611f7fff078
juanfernandez2306 <juanfernandez2306@gmail.com>
2026-09-08 06:02:30 -0400

    se realizo un ajuste en las rutas de strategus

--- src/Strategus/Routes.php
+++ src/Strategus/Routes.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 use App\Middleware\AuthMiddleware;
 use App\Middleware\RoleMiddleware;
 use App\Strategus\Actions\SyncPositionRecordsAction;
-use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
-return function (App $app) {
-    $app->group('/monitoring', function (RouteCollectorProxy $group) {
-        $group->post('/sync', SyncPositionRecordsAction::class);
+return function (RouteCollectorProxy $group): void{
+    $group->group('/monitoring', function (RouteCollectorProxy $monitorinGroup) {
+        $monitorinGroup->post('/sync', SyncPositionRecordsAction::class);
     })
         ->add(AuthMiddleware::class)
         ->add(new RoleMiddleware([2, 3]));
